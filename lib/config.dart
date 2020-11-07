9589001543f9b3637447be30f8dafbaad1fc0ab7
Matthieu GEISS <mgeiss@wevox.eu>
2020-11-07 18:22:00 +0100

    Changement du boolean "prod" à true pour utiliser le serveur en ligne et non pas l'adresse locale

--- lib/config.dart
+++ lib/config.dart
@@ -3,7 +3,7 @@ library config;
 import 'package:cherboug/sales/Sale.dart';
 import 'package:cherboug/shops/shops.dart';
 
-bool prod = false;
+bool prod = true;
 
 String apiURL = prod? "https://cherbourg.wevox.net/index.php" : "http://192.168.1.8/cherbourg/index.php";
 
